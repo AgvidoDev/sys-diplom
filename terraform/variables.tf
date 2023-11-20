082b4a56511942c3b8413ed0ce379d76105b5459
AgvidoDev <136933947+AgvidoDev@users.noreply.github.com>
2023-11-20 21:49:22 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable yc_token {
-    default = "y0_AgAAAABqNtI0AATuwQAAAADrAmKvWGqeARU4R3qXTtqX-JhxBxuSKjs"
+    default = ""
 }
 variable yc_cloud_id {
-    default = "b1g2bqjat09mkusp6mlv"
+    default = ""
 }
 variable yc_folder_id {
-    default = "b1g1pj39942k1kbi4668"
+    default = ""
 }
